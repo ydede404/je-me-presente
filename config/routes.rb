8b9633df76983a86d76d26437ebe6dd3a6c87aef
ydede404 <dakitse@gmail.com>
2017-07-30 13:32:14 +0200

    initialise repository je me présente

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get '/home', to:'static_pages/home'
+
+  get '/contact', to: 'static_pages/contact'
+
+  get '/about', to: 'static_pages/about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
